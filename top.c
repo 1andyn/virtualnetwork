b960988e21fe9c2c104d3627e578b647856fbdef
littlebakemono <andyng@hawaii.edu>
2014-04-03 16:05:26 -1000

    added new code to check first three lines

--- top.c
+++ top.c
@@ -8,6 +8,8 @@
 #define FAIL -1
 #define SUCCESS 1
 #define FILENAMESIZE 100
+
+//Defaults (No Longer Needed)
 #define TREE_SIZE 4
 #define STAR_HOST 3
 #define TREE_HOST 5
@@ -21,21 +23,60 @@ void initTopo(Topo *topology)
 {
    topology->numlinks = 0;
    topology->numhosts = 0;
+   topology->numswitch = 0;
    topology->type = -1;
-}
-
-void initTreeData(char file[], Topo *top)
-{ 
-   printf("You are initing a tree!\n");
-}
-
-void initStarData(char file[], Topo *top)
-{
-   printf("You are initing a star!\n");
 }
 
 void removeNewLine(char *line){
    line[strcspn (line, "\n")] = '\0';
+}
+
+void initTreeData(char file[], Topo *top)
+{ 
+   char *linebuff = NULL; 
+   size_t length = 0;
+   ssize_t linesize;
+   
+   printf("[TREE] Topology detected.\n");
+}
+
+void initStarData(char file[], Topo *top)
+{
+   char *linebuff = NULL; 
+   size_t length = 0;
+   ssize_t linesize;
+
+   printf("[STAR] Topology detected.\n");
+   FILE *data = fopen(file,"r");
+   if(!data){
+      printf("Error: Something wrong occurred!");
+      return;
+   }
+   
+   /* First three lines are NOT Link Data*/
+   int index;
+   for(index = 0; index < 3; index++) {
+      linesize = getline(&linebuff, &length, data);
+      removeNewLine(linebuff);
+      if(linesize != -1){
+         int val = atoi(linebuff);
+         switch(index){
+         case 0: break;
+         case 1: 
+                 top->numhosts = val;
+                 printf("Number of hosts: %d \n", val);
+                 break; 
+         case 2:
+                 top->numswitch = val;
+                 printf("Number of switches: %d \n", val);
+                 break; 
+         }
+      } else {
+         printf("Something went wrong reading first three lines \n");
+         return;
+      }
+   }
+   fclose(data);
 }
 
 int initializeTop(Topo *topology)
